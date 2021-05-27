e09a9396bd8649ef99d6bda33931baf9be6f5fbb
Noah Tajwar <ntajwar@student.ubc.ca>
2021-05-26 18:19:58 -0700

    Working demo with digitizer and AWG

--- Noah/digitizer.py
+++ Noah/digitizer.py
@@ -1,27 +1,48 @@
+# Relevant Documentation: SD1 3.x Software for M310xA / M330xA Digitizers User's Guide
+
 import numpy as np
 
 # Path must be setup before this import
 import keysightSD1
 
-def setup_dig(dig, channel, full_scale, impedance=keysightSD1.AIN_Impedance.AIN_IMPEDANCE_HZ, coupling=keysightSD1.AIN_Coupling.AIN_COUPLING_DC, trigger_pxi_line=1, trigger_mode=keysightSD1.SD_AIN_TriggerMode.FALLING_EDGE):
+DIG_SAMPLE_RATE_MSPS = 500 # MS/s
+
+def setup_dig_pxi(
+    dig,
+    channel,
+    duration_ns,
+    full_scale,
+    prescaler=0,
+    impedance=keysightSD1.AIN_Impedance.AIN_IMPEDANCE_HZ,
+    coupling=keysightSD1.AIN_Coupling.AIN_COUPLING_DC,
+    trigger_pxi_line=1,
+    trigger_behavior=keysightSD1.SD_TriggerBehaviors.TRIGGER_FALL):
+    
+    n_points = int(DIG_SAMPLE_RATE_MSPS * duration_ns // 1000)
+    
+    # Stop and flush DAQ
     dig.daq_stop(channel)
     dig.daq_flush(channel)
-    
-    # TODO:
-    # - full_scale
-    # - prescaler
-    # - trigger_mode
-    
-    
+        
+    # Setup input channel
+    dig.set_prescaler(prescaler, channel)
     dig.set_full_scale(full_scale, channel)
     dig.set_impedance(impedance, channel)
     dig.set_coupling(coupling, channel)
     
-    dig.set_prescaler(prescaler, channel)
-    # TODO not sure about triggers
-    dig.set_digital_trigger_source(trigger_pxi_line, channel)
-    dig.set_digital_trigger_mode(trigger_mode, channel)
+    # Setup DAQ
+    dig.set_points_per_cycle(n_points, channel)
+    dig.set_n_cycles(1, channel)
+    dig.set_daq_trigger_delay(0, channel)
+    dig.set_daq_trigger_mode(keysightSD1.SD_TriggerModes.HWDIGTRIG, channel)
     
-    moduleIn.channelPrescalerConfig(ch, DIG_PRESCALER)
-    moduleIn.DAQdigitalTriggerConfig(ch, pxi1, trigger_mode)
-    moduleIn.DAQconfig(ch, TOT_POINTS_IN, 1, DELAY_IN, ext_trigger)
+    # Setup DAQ digital trigger
+    dig.set_digital_trigger_source(trigger_pxi_line + 4000, channel)
+    dig.set_digital_trigger_behaviour(trigger_behavior, channel)
+    
+    # Setup DAQ read
+    dig.set_n_points(n_points, channel)
+    dig.set_timeout(5000, channel)
+    
+    # Start DAQ
+    dig.daq_start(channel)
